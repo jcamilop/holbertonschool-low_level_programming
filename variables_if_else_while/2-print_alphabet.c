567825ad5d91ba185899615c264f68a3937e4d2a
jcamilop <6410@holbertonstudents.com>
2023-02-22 10:30:16 -0500

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet.
+ *
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	char alp[23] = "abcdefghijklmnopqrstuvwyz"
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	(return) (0)
+}
