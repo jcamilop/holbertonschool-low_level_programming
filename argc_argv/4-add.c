3629c6c778d334451383017e414a82e0a64c5e6f
jcamilop <6410@holbertonstudents.com>
2023-03-10 20:06:31 -0500

    4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -2,45 +2,41 @@
 	#include <stdlib.h>
 	#include <ctype.h>
 	#include <string.h>
-	/**
-	 * check_num - check - string there are digit
-	 * @str: array str
-	 *
-	 * Return: Always 0 (Success)
-	 */
+/**
+* check_num - check - string there are digit
+* @str: array str
+*
+* Return: Always 0 (Success)
+*/
 	int check_num(char *str)
+
+{
+/*Declaring variables*/
+unsigned int count;
+	count = 0;
+while (count < strlen(str)) /*count string*/
+
+{
+	if (!isdigit(str[count])) /*check if str there are digit*/
 	{
-		/*Declaring variables*/
-		unsigned int count;
-
-
-		count = 0;
-		while (count < strlen(str)) /*count string*/
-
-
-		{
-			if (!isdigit(str[count])) /*check if str there are digit*/
-			{
-				return (0);
-			}
-
-
-			count++;
-		}
-		return (1);
+	return (0);
 	}
+	count++;
+}
+return (1);
+}
 
 
-	/**
-	 * main - Print the name of the program
-	 * @argc: Count arguments
-	 * @argv: Arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
+/**
+* main - Print the name of the program
+* @argc: Count arguments
+* @argv: Arguments
+*
+* Return: Always 0 (Success)
+*/
 
 
-	int main(int argc, char *argv[])
+int main(int argc, char *argv[])
 
 
 	{
