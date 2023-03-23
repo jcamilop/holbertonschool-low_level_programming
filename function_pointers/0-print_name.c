abb3f7d732b74be5d951aa86e48b7d1e7d89f8df
jcamilop <6410@holbertonstudents.com>
2023-03-23 13:27:59 -0500

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -4,11 +4,13 @@
 * print_name - print name using pointer to function
 * @name: string to add
 * @f: pointer to function
-*
+* Return: nothing
 **/
-void print_name(char *name, void (*f)(char *))
+	void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+		if (name == NULL || f == NULL)
+			return;
+
 	f(name);
 }
 
