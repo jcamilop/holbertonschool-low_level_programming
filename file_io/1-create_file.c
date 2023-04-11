8432aefc597e819324e792e9d8a93f7eb5a62316
jcamilop <6410@holbertonstudents.com>
2023-04-11 12:34:35 -0500

    function that creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -2,14 +2,18 @@
 
 
 /**
- *a file called filename
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file * Return: If the function fails - -1.
- * Otherwise - 1.
- */
+* create_file - a file called filename
+* @filename: A pointer to the name of the file  to create.
+* @text_content: A pointer to a string to write to the file
+*
+* Return: If the function fails - -1.
+*
+* Otherwise - 1.
+*/
 
 int create_file(const char *filename, char *text_content)
 {
+
 	int fd, w, len = 0;
 
 	if (filename == NULL)
